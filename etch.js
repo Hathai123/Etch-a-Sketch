96c8a7d05c5e019d26fe6191b87e71c1c45b9516
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-06-27 09:20:47 +0700

    mouseover grid make grid change color

--- etch.js
+++ etch.js
@@ -1,9 +1,22 @@
 const container = document.getElementById("container");
 
-addGrid(16*16);
-function addGrid(piece){
-for (let i = 0; i < piece; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "blank");
-    container.appendChild(div);
-}}
+// create 16*16 grid of square divs in conatiner
+addGrid(16 ** 2);
+
+function addGrid(piece) {
+    for (let i = 0; i < piece; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "blank");
+        container.appendChild(div);
+    }
+}
+
+
+const grid = document.querySelectorAll(".blank");
+grid.forEach(function (item) {
+    item.addEventListener("mouseover", event => changeColor(event.currentTarget))
+})
+
+function changeColor(gridBox) {
+    gridBox.style.backgroundColor = "white";
+}
