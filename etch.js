4ccb1ca877a613e9d215156d0059511bc4a024c5
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-06-27 08:52:33 +0700

    test add grid in container

--- etch.js
+++ etch.js
@@ -1,1 +1,9 @@
 const container = document.getElementById("container");
+
+addGrid(50);
+function addGrid(piece){
+for (let i = 0; i < piece; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "blank");
+    container.appendChild(div);
+}}
